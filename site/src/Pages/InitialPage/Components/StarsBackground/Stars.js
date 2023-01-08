476019a751c1ee08f90a7616bb6b79a70a1983cd
unknown <henrique_eduardo_souza@hotmail.com>
2023-01-08 10:49:18 -0300

    fundo estrelado

--- site/src/Pages/InitialPage/Components/StarsBackground/Stars.js
+++ site/src/Pages/InitialPage/Components/StarsBackground/Stars.js
@@ -1,55 +1,24 @@
 import React from 'react';
-import './StarsBackground.css';
+import './StarsBackground.scss';
 
 class Stars extends React.Component{
     constructor(props){
         super(props);
     }
+    
     render(){
         return(
-            <ul className='estrelas'>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        );
+            <div class="main-container">
+                <div class="sub-container">
+                    <div class="sky">
+                        <div class="stars"></div>
+                        <div class="stars2"></div>
+                        <div class="stars3"></div>
+                        <div class="comet"></div>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
