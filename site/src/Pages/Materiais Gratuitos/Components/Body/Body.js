349ef4a2caadd2e4e087984176365ed2c5da44d8
unknown <henrique_eduardo_souza@hotmail.com>
2023-01-17 19:49:53 -0300

    materiais gratuitos básico

--- site/src/Pages/Materiais Gratuitos/Components/Body/Body.js
+++ site/src/Pages/Materiais Gratuitos/Components/Body/Body.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Stars from '../../../InitialPage/Components/StarsBackground/Stars';
 
+import material_01 from '../Body/imgs/material_01.png';
+import material_02 from '../Body/imgs/material_02.png';
+import material_03 from '../Body/imgs/material_03.png';
+import material_04 from '../Body/imgs/material_04.png';
+
 import './Body.css';
 
 class Body extends React.Component{
@@ -13,7 +18,27 @@ class Body extends React.Component{
                 <Stars></Stars>
                 <main class='corpo'>
                     <section>
-                        <h1>dfdfdfd</h1>
+                        <h1>MATERIAIS GRATUITOS</h1>
+                        <p class='basic_text'>Que tal receber uma amostra de nossos materiais?</p>
+                        <div class='img_container'>
+                            <img src={material_01} class='post_img'></img>
+                            <img src={material_02} class='post_img'></img>
+                            <img src={material_03} class='post_img'></img>
+                            <img src={material_04} class='post_img'></img>
+                        </div>
+                        <p class='basic_text'>Veja com teus próprios olhos a qualidade de nosso trabalho!</p>
+                    </section>
+                    <section>
+                        <div class='send_container'>
+                            <h1>RECEBA AGORA!</h1>
+                            <form>
+                                <fieldset class='fields'>
+                                    <input type={'text'} required name='name' placeholder='Digite seu nome'/>
+                                    <input type={'text'} required name='email' placeholder='Digite seu email'/>
+                                </fieldset>
+                            </form>
+                        </div>
+                        
                     </section>
                 </main>
             </>
