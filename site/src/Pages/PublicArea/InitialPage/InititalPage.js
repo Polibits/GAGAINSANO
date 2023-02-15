3a96392b03377795e3e43ba6a1d85507db092533
unknown <henrique_eduardo_souza@hotmail.com>
2023-02-15 15:04:53 -0300

    correção no filepath

--- site/src/Pages/PublicArea/InitialPage/InititalPage.js
+++ site/src/Pages/PublicArea/InitialPage/InititalPage.js
@@ -5,7 +5,7 @@ import HeaderMenor from './Components/NavBar/HeaderMenor';
 import Body from './Components/Body/Body';
 import FooterInicial from './Components/Footer/Footer';
 
-import 'D:\\Projetos\\POLIBITS\\GAGÁ INSANO\\Frontend\\GAGAINSANO\\site\\src\\Pages\\PublicArea\\general.css';
+import './../general.css';
 
 class InitialPage extends React.Component{
     render(){
