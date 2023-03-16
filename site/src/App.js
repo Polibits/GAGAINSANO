60a979eec0b18f6e44b17ff4137b9ad9671e2ddb
unknown <henrique_eduardo_souza@hotmail.com>
2023-03-16 15:40:44 -0300

    mais deleções

--- site/src/App.js
+++ site/src/App.js
@@ -1,27 +1,11 @@
-import './App.css';
 import React from 'react';
 import {Routes, Route, BrowserRouter as Router} from 'react-router-dom';
 
-import InitialPage from "./Pages/PublicArea/InitialPage/InititalPage";
-import Cursos from "./Pages/PublicArea/Cursos/Cursos";
-import Olimpiadas from "./Pages/PublicArea/Olimpíadas/Olimpiadas";
-import MateriaisGratuitos from "./Pages/PublicArea/Materiais Gratuitos/MateriaisGratuitos";
-import GagaInsanoResolve from "./Pages/PublicArea/GagaInsanoResolve/GagaInsanoResolve";
-
-import Login from "./Pages/PublicArea/LoginPage/Login";
-import SignUp from "./Pages/PublicArea/SignUpPage/SignUp";
-
-import Dashboard from './Pages/PrivateArea/Dashboard/Dashboard';
-import CoursePage from './Pages/PrivateArea/CoursePage/CoursePage';
+import InitialPage from './Pages/Public/InitialPage/InitialPage';
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route element = {< InitialPage />}  path="/" exact/>
-        </Routes>
-      </Router>
     </>
   );
 }
---
+++ site/src/Pages/Public/InitialPage/InitialPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class InitialPage extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>InitialPage</h1>
+            </div>
+        );
+    }
+}
+
+export default {
+    InitialPage
+}
