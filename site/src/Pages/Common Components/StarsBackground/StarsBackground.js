18f6b4b0b61245eb0af64ba6bb76bc359136d852
unknown <henrique_eduardo_souza@hotmail.com>
2023-03-17 09:17:57 -0300

    stars background erro de digitação

--- site/src/Pages/Common Components/StarsBackground/StarsBackground.js
+++ site/src/Pages/Common Components/StarsBackground/StarsBackground.js
@@ -8,13 +8,13 @@ class Stars extends React.Component{
     
     render(){
         return(
-            <div class="main-container">
-                <div class="sub-container">
-                    <div class="sky">
-                        <div class="stars"></div>
-                        <div class="stars2"></div>
-                        <div class="stars3"></div>
-                        <div class="comet"></div>
+            <div className="main-container">
+                <div className="sub-container">
+                    <div className="sky">
+                        <div className="stars"></div>
+                        <div className="stars2"></div>
+                        <div className="stars3"></div>
+                        <div className="comet"></div>
                     </div>
                 </div>
             </div>
