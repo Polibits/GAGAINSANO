118310b9f0a34851ab1f89ed6e54ab54c5d70083
unknown <henrique_eduardo_souza@hotmail.com>
2023-03-18 19:38:17 -0300

    criação de framework de cursos

--- site/src/Pages/Private/adm/courses/AdmCourses.js
+++ site/src/Pages/Private/adm/courses/AdmCourses.js
@@ -1,17 +1,104 @@
 import React from "react";
+import axios from "axios";
 
 class AdmCourses extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { coursesFrameworks:[], loaded:false};
+    }
+
+    getCoursesFrameworks = () => {
+        axios({
+            method:'get',
+            url:'http://localhost:5050/courses/framework/read/all'
+        }).then((response) => {
+            this.setState({coursesFrameworks: response.data.coursesFrameworks});
+        });
     }
     
     render(){
         return (
             <div className='AdmCoursesPage'>
                 <h1>AdmCoursesPage</h1>
+                <div className="Forms">
+                    <form>
+                        {Field('Nome Comercial do Curso', 'comercialName', 'text')}
+                        {Field('Código do curso', 'courseCode', 'text')}
+                        {Field('Descrição breve', 'description', 'textarea')}
+                        {Field('Preço', 'price', 'text')}
+                        <div>
+                            <label>Frequência de Pagamento</label>
+                        </div>
+                        <select id="paymentFrequency">
+                            <option value="single">única</option>
+                            <option value="monthly">mensal</option>
+                            <option value="annually">anual</option>
+                        </select>
+                    </form>
+                </div>
+                <button onClick={createCourse}>criar curso</button>
+                <button onClick={this.getCoursesFrameworks}>carregar cursos</button>
+                <div>
+                    {CoursesFrame(this.state.coursesFrameworks)}
+                </div>
             </div>
         );
     }
+}
+
+function createCourse() {
+    const courseCode = document.getElementById("courseCode").value;
+    const comercialName = document.getElementById("comercialName").value;
+    const description = document.getElementById("description").value;
+    const price = document.getElementById("price").value;
+    const paymentFrequency = document.getElementById("paymentFrequency").value;
+
+    const courseFramework = {
+        'comercialName':comercialName,
+        'courseCode':courseCode,
+        'description':description,
+        'price':price,
+        'paymentFrequency':paymentFrequency
+    };
+    axios({
+        method:'post',
+        url:'http://localhost:5050/courses/framework/create',
+        data:courseFramework
+    }).then((response) => {
+        console.log(response.data);
+    });
+}
+
+function CoursesFrame(courses) {
+    var list = []
+    console.log('fffffffffffffffff', courses);
+
+    return (
+        <div>
+            {courses.map((course) => {
+                return (
+                    <div key={course.id}>
+                        <p>comercialName: {course.comercialName}</p>
+                        <p>courseCode: {course.courseCode}</p>
+                        <p>description: {course.description}</p>
+                        <p>price: {course.price}</p>
+                        <p>paymentFrequency: {course.paymentFrequency}</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
+
+function Field(name, id, type) {
+    return (
+        <div className="Field">
+            <p>{name}</p>
+            <label>
+                <input id={id} type={type}></input>
+            </label>
+        </div>
+    );
 }
 
 export default AdmCourses;
