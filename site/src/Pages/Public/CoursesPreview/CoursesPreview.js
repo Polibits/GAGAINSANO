dd974e4ea9f288864892535eb53e3c447b4de812
henrique eduardo <henrique_eduardo_souza@hotmail.com>
2023-03-25 14:39:05 -0300

    sla sem ideia para msg de commit

--- site/src/Pages/Public/CoursesPreview/CoursesPreview.js
+++ site/src/Pages/Public/CoursesPreview/CoursesPreview.js
@@ -1,4 +1,6 @@
 import React from "react";
+
+import courseImg from '../../../content/Logo.png';
 import StarsBackground from '../../Common Components/StarsBackground/StarsBackground';
 import NavigationBar from "../../Common Components/NavigationBar/NavigationBar";
 import './../../GlobalStyle.css';
@@ -14,11 +16,76 @@ class CoursesPreview extends React.Component {
             <div className='Page' id='CoursesPreviewPage'>
                 <div id='PageContent'>
                     <NavigationBar/>
-                    <h1>cursos</h1>
+                    <h1>CURSOS</h1>
+                    <div className="body">
+                        <MilitaryCourses/>
+                        <VestCourses/>
+                        <OlimpicCourses/>
+                    </div>
                 </div>
                 <div id='PageBackground'>
                     <StarsBackground/>
                 </div>
+            </div>
+        );
+    }
+}
+
+class MilitaryCourses extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render(){
+        return (
+            <div className="MilitaryCourses">
+                <h2 className="SectionTitle">Vestibulares Militares</h2>
+            </div>
+        );
+    }
+}
+
+class VestCourses extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render(){
+        return (
+            <div className="VestCourses">
+                <h2 className="SectionTitle">Vestibulares Tradicionais</h2>
+            </div>
+        );
+    } 
+}
+
+class OlimpicCourses extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render(){
+        return (
+            <div className="OlimpicCourses">
+                <h2 className="SectionTitle">Olimpíadas</h2>
+            </div>
+        );
+    }
+}
+
+class CoursePresentation extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render(){
+        return (
+            <div className="CoursePresentation">
+                <div className="Img">
+                    <img src={this.props.img}/>
+                </div>
+                <h3>{this.props.courseTitle}</h3>
+                
             </div>
         );
     }
