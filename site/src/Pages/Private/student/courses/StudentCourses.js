91ddf94c2f441da041ff39082ef16c668085c8a7
henrique eduardo <henrique_eduardo_souza@hotmail.com>
2023-03-26 11:05:12 -0300

    student courses videoframe

--- site/src/Pages/Private/student/courses/StudentCourses.js
+++ site/src/Pages/Private/student/courses/StudentCourses.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+import StarsBackground from '../../../Common Components/StarsBackground/StarsBackground.js';
+
+import '../../../GlobalStyle.css';
+import './StudentCourses.css';
+
 class StudentCourses extends React.Component {
     constructor(props) {
         super(props);
@@ -7,10 +12,58 @@ class StudentCourses extends React.Component {
     
     render(){
         return (
-            <div className='Page'>
-                <div id='StudentCourseLateralMenu'></div>
-                <div id='StudentCourseLateralMenu'></div>
+            <div className='Page' id='StudentCoursesPage'>
+                <div id='PageContent'>
+                    <Lecture title='Aula 01 - Leis de Newton' src='https://www.youtube.com/embed/6sBkmAlJlvs'/>
+                </div>
+                <div id='PageBackground'>
+                    <StarsBackground/>
+                </div>
             </div>
+        );
+    }
+}
+
+class Lecture extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="Lecture">
+                <div className="LectureBox">
+                    <div className="ComponentsDiv">
+                        <div className="Title">
+                            <h2>{this.props.title}</h2>
+                        </div>
+                        <div className="Frame">
+                            <div className="Video">
+                                <LectureVideo src={this.props.src}/>
+                            </div>
+                            <div className="Navigation">
+                                <a className="Previous" to={this.props.previousLecture}>anterior</a>
+                                <a className="Next" to={this.props.nextLecture}>próxima</a>
+                            </div>
+                        </div>
+                        <div className="description">
+                            <p>{this.props.description}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class LectureVideo extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <iframe src={this.props.src}></iframe>
         );
     }
 }
