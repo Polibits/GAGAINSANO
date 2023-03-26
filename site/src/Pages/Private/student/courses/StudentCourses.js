ecd3745ae19b1706bff8c22e835e5699b85f9ae9
henrique eduardo <henrique_eduardo_souza@hotmail.com>
2023-03-26 15:35:24 -0300

    to sem criatividade para msg de commit

--- site/src/Pages/Private/student/courses/StudentCourses.js
+++ site/src/Pages/Private/student/courses/StudentCourses.js
@@ -2,8 +2,14 @@ import React from "react";
 
 import StarsBackground from '../../../Common Components/StarsBackground/StarsBackground.js';
 
+import example from '../../../../content/videos/example.mp4';
+
 import '../../../GlobalStyle.css';
 import './StudentCourses.css';
+
+// https://www.youtube.com/watch?v=DBXH9jJRaDk
+const url = 'https://www.youtube.com/watch?v=STXM8uXWCnQ';
+const description = 'Nesta aula, aprendemos sobre as leis de newton';
 
 class StudentCourses extends React.Component {
     constructor(props) {
@@ -14,10 +20,27 @@ class StudentCourses extends React.Component {
         return (
             <div className='Page' id='StudentCoursesPage'>
                 <div id='PageContent'>
-                    <Lecture title='Aula 01 - Leis de Newton' src='https://www.youtube.com/embed/6sBkmAlJlvs'/>
+                    <Lecture title='Aula 01 - Leis de Newton' src={example} description={description}/>
                 </div>
                 <div id='PageBackground'>
                     <StarsBackground/>
+                </div>
+            </div>
+        );
+    }
+}
+
+class LateralMenu extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="LateralMenu">
+                <div className="CourseProfile"></div>
+                <div className="MenuOptions">
+                    <div className="Index"></div>
                 </div>
             </div>
         );
@@ -45,10 +68,12 @@ class Lecture extends React.Component {
                                 <a className="Previous" to={this.props.previousLecture}>anterior</a>
                                 <a className="Next" to={this.props.nextLecture}>próxima</a>
                             </div>
+                            <div className="description">
+                                <p>descrição</p>
+                                <p>{this.props.description}</p>
+                            </div>
                         </div>
-                        <div className="description">
-                            <p>{this.props.description}</p>
-                        </div>
+                        
                     </div>
                 </div>
             </div>
@@ -56,14 +81,57 @@ class Lecture extends React.Component {
     }
 }
 
+function videoURL(youtubeURL) {
+    var finalURL = '';
+    var id = '';
+    var URLendStart = 32;
+
+    for(var i = URLendStart; i < youtubeURL.length; i++){
+        id += youtubeURL[i];
+    }
+
+    /*
+    finalURL += 'https://www.youtube-nocookie.com/embed/';
+    finalURL += id;
+    finalURL += '?modestbranding=1'
+    finalURL += '&showinfo=0';
+    finalURL += '&controls=1';
+    finalURL += '&showinfo=0';
+    */
+    finalURL += 'https://www.youtube-nocookie.com/embed/';
+    finalURL += id;
+    finalURL += '?controls=0&mode=opaque&amp;rel=0&amp;autohide=1&amp;showinfo=0&amp;wmode=transparent';
+    // ?mode=opaque
+    //&amp;
+    //rel=0
+    //&amp;autohide=1&amp;showinfo=0&amp;wmode=transparent
+    //?mode=opaque&amp;rel=0&amp;autohide=1&amp;showinfo=0&amp;wmode=transparent
+
+    return finalURL;
+}
+
+function removeSharingOptions() {
+    console.log('removendo');
+    try {
+        //const element = document.querySelector("#document");
+        //element.remove();
+        //var iframe_document = document.getElementById('lectureVideoIframe').contentWindow.document;
+    } catch (error) {
+        //console.log(error);
+    }
+}
+
 class LectureVideo extends React.Component {
     constructor(props) {
         super(props);
     }
-
+    
     render() {
         return (
-            <iframe src={this.props.src}></iframe>
+            <div>
+                <video src={this.props.src} id='lectureVideoIframe' controls controlsList="nodownload"/>
+            </div>
+            
         );
     }
 }
