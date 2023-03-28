fe989a2965b967fbeeadd9f4e62a07968bb98d70
henrique eduardo <henrique_eduardo_souza@hotmail.com>
2023-03-28 08:18:03 -0300

    StudentCourseInfo

--- site/src/Pages/Private/student/courses/Information/StudentInformation.js
+++ site/src/Pages/Private/student/courses/Information/StudentInformation.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import profilePicture from '../../../../../content/Logo.png';
+
+import './StudentInformation.css';
+
 class CourseInformation extends React.Component {
     constructor(props){
         super(props);
@@ -7,8 +11,16 @@ class CourseInformation extends React.Component {
 
     render() {
         return (
-            <div>
-                <Description name='Física para Militares'/>
+            <div id='StudentCourseInformation'>
+                <Description 
+                    name='Física para Militares'
+                    src={profilePicture}
+                    resume='O curso de Física para Militares é o melhor curso de física do universo'
+                    courseCode='fisica_militares'
+                    price={'R$ 49.99'}
+                    paymentFrequency='mensal'
+                    content={<Content/>}
+                />
             </div>
         );
     }
@@ -22,22 +34,55 @@ class Description extends React.Component {
     render() {
         return (
             <div className="Description">
-                <h1 className="CourseTitle">Informação do Curso</h1>
-                <div>
-                    <h2>{this.props.name}</h2>
-                    <div>
-                        <img src={this.src}/>
+                <div className="GeneralSection">
+                    <h2 className="CourseTitle">{this.props.name}</h2>
+                    <div className="CourseImgDiv">
+                        <img src={this.props.src}/>
                     </div>
-                    <p>{this.description}</p>
-                    <p>{this.content}</p>
+                    <div className="ResumeDiv">
+                        <p className="Resume">{this.props.resume}</p>
+                    </div>
+                    
                 </div>
-                <h1>Assinatura</h1>
-                <div>
-                    <p>código: {}</p>
-                    <p>preço: {}</p>
-                    <p>frequência de pagamento:{}</p>
-                    <button>cancelar assinatura</button>
+                <div className="SpecificSection">
+                    <div className="Content">
+                        <p>{this.props.content}</p>
+                    </div>
+                    <div>
+                        <h2>Assinatura</h2>
+                        <p>código: {this.props.courseCode}</p>
+                        <p>preço: {this.props.price}</p>
+                        <p>frequência de pagamento: {this.props.paymentFrequency}</p>
+                        <button>cancelar assinatura</button>
+                    </div>
+                    
                 </div>
+            </div>
+        );
+    }
+}
+
+class Content extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Ementa</h2>
+                <p>Aula 01 - Leis de newton</p>
+                <p>Aula 01 - Leis de newton</p>
+                <p>Aula 01 - Leis de newton</p>
+                <p>Aula 01 - Leis de newton</p>
+                <p>Aula 01 - Leis de newton</p>
+                <p>Aula 01 - Leis de newton</p>
+                <p>Aula 01 - Leis de newton</p>
+                <p>Aula 01 - Leis de newton</p>
+                <p>Aula 01 - Leis de newton</p>
+                <p>Aula 01 - Leis de newton</p>
+                <p>Aula 01 - Leis de newton</p>
+                <p>Aula 01 - Leis de newton</p>
             </div>
         );
     }
